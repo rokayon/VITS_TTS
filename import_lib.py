aea78c23fc2a9441e8b1afff67bbe82b3bfc5fb9
rokayon <rokayon.cse@gmail.com>
2026-01-20 15:38:39 +0600

    Added import libray Script

--- import_lib.py
+++ import_lib.py
@@ -1,26 +1,18 @@
 # !nvidia-smi
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
+import numpy as np 
+import pandas as pd 
 import os
 import torch
-# Trainer: Where the ✨️ happens.
-# TrainingArgs: Defines the set of arguments of the Trainer.
 from trainer import Trainer, TrainerArgs
 
 from TTS.tts.configs.vits_config import VitsConfig
 from TTS.tts.models.vits import Vits, VitsAudioConfig
-# BaseDatasetConfig: defines name, formatter and path of the dataset.
-
 
 from TTS.tts.utils.text.tokenizer import TTSTokenizer
 from TTS.utils.audio import AudioProcessor
 
 from TTS.tts.configs.shared_configs import BaseDatasetConfig,BaseAudioConfig,CharactersConfig
-#from TTS.configs import BaseDatasetConfig,BaseAudioConfig,CharactersConfig#GlowTTSConfig
+
 from TTS.tts.datasets import load_tts_samples
 from TTS.tts.utils.speakers import SpeakerManager
 
